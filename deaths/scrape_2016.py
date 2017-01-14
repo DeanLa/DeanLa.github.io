3dd2cd8f48ed339549ad5ced49da98ef57f3e973
Dean Langsam <deanla@gmail.com>
2017-01-14 22:28:54 +0200

    stock bg image

--- deaths/scrape_2016.py
+++ deaths/scrape_2016.py
@@ -1,12 +1,12 @@
 import pandas as pd
 from bs4 import BeautifulSoup
 import requests
-
+from calendar import month_name
+import sys
 r = requests.get('https://en.wikipedia.org/wiki/Deaths_in_January_2016')
 print(r.status_code)
 soup = BeautifulSoup(r.content, 'lxml')
 days_of_month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-months = {"January"}
 
 year = []
 month = []
@@ -14,6 +14,8 @@ day = []
 html = []
 deaths = []
 
+for mn in month_name:
+    pass
 for d in range(1, days_of_month[0] + 1):
     day_current = soup.find(class_='mw-headline', id=d)
     if day_current is None:
