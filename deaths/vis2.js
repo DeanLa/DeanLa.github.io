5090435424e4cea33129f3f238f86e6a81b881bb
Dean Langsam <deanla@gmail.com>
2017-02-02 00:58:30 +0200

    Design Improvments

--- deaths/vis2.js
+++ deaths/vis2.js
@@ -4,8 +4,8 @@
 var margin = {
     top: 10,
     right: 20,
-    bottom: 30,
-    left: 40
+    bottom: 50,
+    left: 60
 };
 var width = 1000 - margin.left - margin.right;
 var height = 500 - margin.top - margin.bottom;
@@ -34,6 +34,22 @@ svg
     .append('g')
     .call(d3.axisLeft(yScale));
 
+// Labels
+svg.append("text")
+    .classed("legend", true)
+    .attr("transform",
+        `translate(${width / 2} ,${height + margin.top + 30})`)
+    .style("text-anchor", "middle")
+    .text("Date 2016");
+
+svg.append("text")
+    .classed("legend", true)
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left)
+    .attr("x", 0 - (height / 2))
+    .attr("dy", "1em")
+    .style("text-anchor", "middle")
+    .text("Total Celebrity Deaths");
 
 const type = function (data) {
     data.forEach(d => {
@@ -44,11 +60,8 @@ const type = function (data) {
 };
 
 var line = d3.line()
-// .x(d => xScale(d['date_in_year']))
-// .y(d => yScale(d['total_deaths']))
     .x(d => xScale(d.date_in_year))
     .y(d => yScale(d.total_deaths))
-// .curve(d3.curveCatmullRom.alpha(0.5));
 
 
 d3.csv('./2016.csv', function (err, data) {
@@ -66,19 +79,18 @@ d3.csv('./2017.csv', function (err, data) {
         .attr('class', 'line total y2017')
         .attr('d', line(data));
 });
-
 for (i = 0; i <= 1; i++) {
-    y_pos = 20*(i+1);
+    y_pos = 20 * (i + 1);
     svg
         .append('path')
         .attr('d', `M50,${y_pos}L100,${y_pos}`)
-        .attr('class', 'line y'+(i+2016));
+        .attr('class', 'line y' + (i + 2016));
     svg
         .append('text')
-        .classed ('legend', true)
+        .classed('legend', true)
         .attr('x', '110')
-        .attr('y', 5+y_pos)
-        .text(''+(i+2016));
+        .attr('y', 5 + y_pos)
+        .text('' + (i + 2016));
 }
 
 function responsivefy(svg) {
