9ddde122ed67742739f9bb0cafdc9dc04c0fa7b9
Dean Langsam <deanla@gmail.com>
2018-03-23 12:04:05 +0300

    Update documentation

---
+++ apps/js/fbui.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Dean on 16-Jan-17.
+ */
+window.fbAsyncInit = function () {
+    FB.init({
+        appId: '259760160731955',
+        xfbml: true,
+        version: 'v2.8'
+    });
+    FB.AppEvents.logPageView();
+};
+
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {
+        return;
+    }
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
---
+++ apps/valley/name_change.js
@@ -0,0 +1,6 @@
+// Name Changes
+// Add .io, .fm, .js, py
+// in the end: ir, er => r
+// c => k or q or c
+// i => y
+// if I as first letter => change to i and captalize next
