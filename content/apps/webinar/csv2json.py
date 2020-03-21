85892320ea1581baa633452cef2cd9258902c050
deanla@gmail.com <deanla@gmail.com>
2020-03-21 13:03:25 +0200

    webinars

--- content/apps/webinar/csv2json.py
+++ content/apps/webinar/csv2json.py
@@ -5,7 +5,7 @@ df = pd.read_csv('data.csv', delimiter='\t')
 
 d = df.to_dict('series')
 print(d.keys())
-d = {k: v.dropna().tolist() for k, v in d.items()}
+d = {k: v.str.strip(' ').dropna().tolist() for k, v in d.items()}
 # d['type']
 # d['establishment_prefix']
 with open('webinar.json','w', encoding='latin-1') as f:
